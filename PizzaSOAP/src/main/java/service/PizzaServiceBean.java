d6945b2a4216d1e068c2b6867fc0193804b3bbca
peaseloxes <peaseloxes@gmail.com>
2015-11-17 02:14:58 -0600

    Pizza support added

--- PizzaSOAP/src/main/java/service/PizzaServiceBean.java
+++ PizzaSOAP/src/main/java/service/PizzaServiceBean.java
@@ -1,8 +1,12 @@
 package service;
 
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import pizza.domain.concrete.persist.Pizza;
+import pizza.repository.PizzaRepository;
 
 /**
  * Created by alex on 11/16/15.
@@ -10,8 +14,12 @@ import javax.jws.WebService;
 @Stateless
 @WebService
 public class PizzaServiceBean {
+
+    @EJB
+    private PizzaRepository pizzaRepository;
+
     @WebMethod
-    public String getPizzas() {
-        return "pizza";
+    public List<Pizza> getPizzas() {
+        return pizzaRepository.getAll();
     }
 }
