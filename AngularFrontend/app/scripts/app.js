1e902d5623dfac15a04be3d395d11d37ec26074f
peaseloxes <peaseloxes@gmail.com>
2015-12-03 06:46:19 -0600

    yo angular

---
+++ AngularFrontend/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pizzaApp
+ * @description
+ * # yeomanGeneratedProjectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pizzaApp', [
+    'ngAnimate'
+  ]);
---
+++ AngularFrontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanGeneratedProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanGeneratedProjectApp
+ */
+angular.module('pizzaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
